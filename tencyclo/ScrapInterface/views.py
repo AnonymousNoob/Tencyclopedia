7f1ce2d318fe8d9ec0438a3968d584d2f653d91f
Prawin <theincredible13@gmail.com>
2016-09-18 21:52:52 +0530

    Scrapped HotHits

--- tencyclo/ScrapInterface/views.py
+++ tencyclo/ScrapInterface/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+ 
+from bs4 import BeautifulSoup
+import requests
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
 
 # Create your views here.
 def index(request):
-	return render(request,"index.html",{})
+	r  = requests.get("http://www.gsmarena.com/news.php3")
+
+	data = r.text
+
+	soup = BeautifulSoup(data,"html.parser")
+	soup.prettify()
+	mydivs = soup.find_all("nobr")
+
+	names=[]
+	for name in mydivs:
+		names.append(name.getText())
+	    
+
+	return TemplateResponse(request,"index.html",{'ob':names})
